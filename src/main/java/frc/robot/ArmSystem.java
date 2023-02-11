c6446208417bd5a5fe51f93fcdeb4693500a35b0
Chaotic-V01D <87347377+Chaotic-V01D@users.noreply.github.com>
2023-02-11 13:49:05 -0600

    Additions to arm system
    Added new variables to access arm extension and claw initiation
    Added implementation for arm extension when triggered

--- src/main/java/frc/robot/ArmSystem.java
+++ src/main/java/frc/robot/ArmSystem.java
@@ -3,7 +3,11 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 public class ArmSystem {
     private static WPI_TalonFX zRot = new WPI_TalonFX(5);
+    private static WPI_TalonFX armExtend = new WPI_TalonFX(6);
     public void turnArm(double turnSpeed) {
         zRot.set(turnSpeed);
     };
+    public void extendArm(double extendSpeed) {
+        armExtend.set(extendSpeed);
+    }
 }
